0a09299daaf0b6bbb6ea38819b6182267da67bdd
SorerBOT <alonfiller@gmail.com>
2024-06-29 16:05:00 +0300

    (Feat) Var is now assigned and evaluated properly

--- src/Var.java
+++ src/Var.java
@@ -7,30 +7,13 @@ import java.util.ArrayList;
  */
 public final class Var implements Expression {
     private final String name;
-    private final Expression expression;
 
-    /**
-     * Constructor of the Var class.
-     * @param name the name of the Var
-     * @param expression the expression which is represented by the Var.
-     */
-    public Var(String name, Expression expression) {
-        this.name = new String(name);
-        this.expression = expression;
-    }
-    /**
-     * Copy constructor of the Var class.
-     * @param var the Var instance to be copied
-     */
-    public Var(Var var) {
-        this(var.getName(), var.getExpression());
-    }
     /**
      * Constructor of the Var class.
      * @param name the name of the Var
      */
     public Var(String name) {
-        this(name, null);
+        this.name = new String(name);
     }
 
     /**
@@ -40,28 +23,17 @@ public final class Var implements Expression {
     public String getName() {
         return new String(this.name);
     }
-    /**
-     * Getter of the expression field.
-     * @return the expression field
-     */
-    public Expression getExpression() {
-        return this.expression;
-    }
     @Override
     public Boolean evaluate(Map<String, Boolean> assignment) throws Exception {
-        final Boolean newValue = this.expression.evaluate(assignment);
-        for (Map.Entry<String, Boolean> entry : assignment.entrySet()) {
-            if (!this.name.equals(entry.getKey())) {
-                continue;
-            }
-            entry.setValue(newValue);
+        if (assignment.containsKey(this.name)) {
+            return assignment.get(this.name);
         }
-        return newValue;
+        throw new Exception("Var " + this.toString() + " is not assigned");
     }
 
     @Override
     public Boolean evaluate() throws Exception {
-        return this.expression.evaluate();
+        throw new Exception("Var " + this.toString() + " is not assigned");
     }
 
     @Override
@@ -82,6 +54,6 @@ public final class Var implements Expression {
         if (this.name.equals(var)) {
             return expression;
         }
-        return new Var(this);
+        return new Var(this.name);
     }
 }
