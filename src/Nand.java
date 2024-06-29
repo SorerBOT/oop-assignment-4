60e70c16a767c88cbc74440335a99b10151a7de3
SorerBOT <alonfiller@gmail.com>
2024-06-29 15:13:03 +0300

    (Feat) nand

---
+++ src/Nand.java
@@ -0,0 +1,24 @@
+/**
+ * The Nand class.
+ */
+public class Nand extends BinaryExpression {
+    /**
+     * Constructor of the Nand class.
+     * @param firstExpression the first Expression
+     * @param secondExpression the second Expression
+     */
+    public Nand(Expression firstExpression, Expression secondExpression) {
+        super(firstExpression, secondExpression, "A");
+    }
+    @Override
+    public Boolean handleOperation(Boolean firstOperand, Boolean secondOperand) {
+        return !(firstOperand && secondOperand);
+    }
+    @Override
+    public Expression assign(String var, Expression expression) {
+        return new Nand(
+            this.getFirstExpression().assign(var, expression),
+            this.getSecondExpression().assign(var, expression)
+        );
+    }
+}
