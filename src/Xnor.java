44392074a7382e60073ef3365c8744be25ebd306
SorerBOT <alonfiller@gmail.com>
2024-06-29 17:58:54 +0300

    (Feat) norified Xnor

--- src/Xnor.java
+++ src/Xnor.java
@@ -30,4 +30,8 @@ public class Xnor extends BinaryExpression {
             ).nandify()
         ).nandify();
     }
+    @Override
+    public Expression norify() {
+        return this.nandify().norify();
+    }
 }
