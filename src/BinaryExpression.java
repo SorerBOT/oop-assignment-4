32ba74ea35428dc6cc76887d9ba70c17629e68e7
SorerBOT <alonfiller@gmail.com>
2024-06-29 14:40:01 +0300

    (Feat) turned the code more generic

--- src/BinaryExpression.java
+++ src/BinaryExpression.java
@@ -1,14 +1,79 @@
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.LinkedHashSet;
+
 /**
  * The BinaryExpression class.
  */
-public abstract class BinaryExpression {
+public abstract class BinaryExpression implements Expression {
     private final Expression firstExpression;
     private final Expression secondExpression;
     private final String operator;
 
     /**
-     * Applies the binary operation to the expressions.
-     * @return
+     * The constructor of the BinaryExpression class.
+     * @param firstExpression the first operand of the BinaryExpression
+     * @param secondExpression the second operand of the BinaryExpression
+     * @param operator the symbol of the operator to be used
      */
-    public abstract Boolean handleOperation();
+    public BinaryExpression(Expression firstExpression, Expression secondExpression, String operator) {
+        this.firstExpression = firstExpression;
+        this.secondExpression = secondExpression;
+        this.operator = new String(operator);
+    }
+
+    /**
+     * Applies the binary operation to the operands.
+     * @param firstOperand the first operand
+     * @param secondOperand the second operand
+     * @return the result of the binary operation
+     */
+    public abstract Boolean handleOperation(Boolean firstOperand, Boolean secondOperand);
+    /**
+     * Getter of the firstExpression field.
+     * @return the firstExpression field
+     */
+    public Expression getFirstExpression() {
+        return this.firstExpression;
+    }
+    /**
+     * Getter of the secondExpression field.
+     * @return the secondExpression field
+     */
+    public Expression getSecondExpression() {
+        return this.secondExpression;
+    }
+    @Override
+    public Boolean evaluate(Map<String, Boolean> assignment) throws Exception {
+        return this.handleOperation(
+            this.firstExpression.evaluate(assignment),
+            this.secondExpression.evaluate(assignment)
+        );
+    }
+
+    @Override
+    public Boolean evaluate() throws Exception {
+        return this.handleOperation(
+            this.firstExpression.evaluate(),
+            this.secondExpression.evaluate()
+        );
+    }
+
+    @Override
+    public List<String> getVariables() {
+        Set<String> set = new LinkedHashSet<>(this.firstExpression.getVariables());
+        set.addAll(this.secondExpression.getVariables());
+
+        return new ArrayList<String>(set);
+    }
+
+    @Override
+    public String toString() {
+        return this.firstExpression.toString() + this.operator + this.secondExpression.toString();
+    }
+
+    @Override
+    public abstract Expression assign(String var, Expression expression);
 }
