a969250ec9dbd9d72414160931e9b3a431dffcaa
SorerBOT <alonfiller@gmail.com>
2024-06-29 15:15:20 +0300

    (Feat) nor

---
+++ src/Nor.java
@@ -0,0 +1,24 @@
+/**
+ * The Nor class.
+ */
+public class Nor extends BinaryExpression {
+    /**
+     * Constructor of the Nor class.
+     * @param firstExpression the first Expression
+     * @param secondExpression the second Expression
+     */
+    public Nor(Expression firstExpression, Expression secondExpression) {
+        super(firstExpression, secondExpression, "V");
+    }
+    @Override
+    public Boolean handleOperation(Boolean firstOperand, Boolean secondOperand) {
+        return !(firstOperand || secondOperand);
+    }
+    @Override
+    public Expression assign(String var, Expression expression) {
+        return new Nor(
+            this.getFirstExpression().assign(var, expression),
+            this.getSecondExpression().assign(var, expression)
+        );
+    }
+}
